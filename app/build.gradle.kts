86ad6268bd1901254d078fd3f8cd395093a1de1d
Philipp Craine <125037167+philippzhuravlev@users.noreply.github.com>
2025-09-19 10:21:26 +0200

    Integrate Firebase and Google services plugins
    Added the Google services Gradle plugin and Firebase dependencies to enable Firebase integration. Updated build scripts to use explicit plugin IDs and included the Firebase BoM and analytics library.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
     alias(libs.plugins.kotlin.android)
 }
 
@@ -39,6 +41,16 @@ android {
 }
 
 dependencies {
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available_libraries
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
