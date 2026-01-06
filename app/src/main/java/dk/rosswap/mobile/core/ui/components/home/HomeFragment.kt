f807d73f389db08d1810262362cea44c3e0bf20d
akkash0802 <s246544@dtu.dk>
2026-01-06 14:37:17 +0100

    navigation

--- app/src/main/java/dk/rosswap/mobile/core/ui/components/home/HomeFragment.kt
+++ app/src/main/java/dk/rosswap/mobile/core/ui/components/home/HomeFragment.kt
@@ -1,18 +1,20 @@
 package dk.rosswap.mobile.core.ui.components.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import dk.rosswap.mobile.R
 import dk.rosswap.mobile.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,22 +22,49 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSwipe.setOnClickListener {
+            Toast.makeText(requireContext(), "Swipe Posts clicked", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "Swipe Posts clicked")
+            // TODO: navigate to swipe screen
         }
-        return root
+
+        binding.btnNewPosts.setOnClickListener {
+            Toast.makeText(requireContext(), "See New Posts clicked", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "See New Posts clicked")
+            // TODO: navigate to new posts screen
+        }
+
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(requireContext(), "Log In clicked", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "Log In clicked")
+            // TODO: open login fragment / activity
+            // navigates using the action defined in `mobile_navigation.xml`
+            findNavController().navigate(R.id.action_nav_home_to_loginFragment)
+        }
+
+        binding.btnCreatePost.setOnClickListener {
+            Toast.makeText(requireContext(), "Create Post clicked", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "Create Post clicked")
+            // TODO: open create post screen
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-}
 
+    companion object {
+        private const val TAG = "HomeFragment"
+
+        @JvmStatic
+        fun newInstance(): HomeFragment = HomeFragment()
+    }
+}
