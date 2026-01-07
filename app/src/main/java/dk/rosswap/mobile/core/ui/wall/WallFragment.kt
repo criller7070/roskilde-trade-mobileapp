82bc8f8d030f1a77809c5b0627ec1c431ebf1ba0
lundHannah <hannahensafilund@gmail.com>
2026-01-07 15:27:44 +0100

    Add WallFragment and ViewModel, implement RecyclerView for posts

---
+++ app/src/main/java/dk/rosswap/mobile/core/ui/wall/WallFragment.kt
@@ -0,0 +1,33 @@
+// Kotlin
+package dk.rosswap.mobile.core.ui.wall
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dk.rosswap.mobile.databinding.FragmentWallBinding
+
+class WallFragment : Fragment() {
+
+    private var _binding: FragmentWallBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var recycler: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWallBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
