3b6e410772cf616b112ff61dbab4a620a606b17b
Lilianosman <osmanlilian16@gmail.com>
2026-01-08 15:23:08 +0100

    privacy

---
+++ app/src/main/java/dk/rosswap/mobile/core/ui/components/privacy/PrivacyPolicyFragment.kt
@@ -0,0 +1,31 @@
+package dk.rosswap.mobile.ui.components.privacy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dk.rosswap.mobile.databinding.FragmentPrivacyPolicyBinding
+
+class PrivacyPolicyFragment : Fragment() {
+
+    private var _binding: FragmentPrivacyPolicyBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: PrivacyPolicyViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPrivacyPolicyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/dk/rosswap/mobile/core/ui/components/privacy/PrivacyPolicyViewModel.kt
@@ -0,0 +1,5 @@
+package dk.rosswap.mobile.ui.components.privacy
+
+import androidx.lifecycle.ViewModel
+
+class PrivacyPolicyViewModel : ViewModel()
