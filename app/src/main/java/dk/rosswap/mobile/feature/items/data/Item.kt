31e498f520830cc245fc639ff0449273d112145e
Philipp Craine <125037167+philippzhuravlev@users.noreply.github.com>
2026-01-10 20:33:41 +0100

    rename to impl

---
+++ app/src/main/java/dk/rosswap/mobile/feature/items/data/Item.kt
@@ -0,0 +1,14 @@
+    package dk.rosswap.mobile.feature.items.data
+
+    import com.google.firebase.Timestamp
+
+    data class Item(
+        val id: String,
+        val title: String,
+        val description: String,
+        val mode: String,
+        val imageUrl: String,
+        val userId: String,
+        val userName: String,
+        val createdAt: Timestamp?
+    )
