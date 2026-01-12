940429ebcece07f13213a791b20a175da2abb4a6
Oliver Gram Lazarotti <89944418+M3n3ck@users.noreply.github.com>
2026-01-12 16:24:17 +0100

    Add getDislikedItems to LikedRepository interface

--- app/src/main/java/dk/rosswap/mobile/feature/liked/domain/LikedRepository.kt
+++ app/src/main/java/dk/rosswap/mobile/feature/liked/domain/LikedRepository.kt
@@ -4,4 +4,5 @@ import dk.rosswap.mobile.feature.items.domain.Item
 
 interface LikedRepository {
     suspend fun getLikedItems(userId: String): Result<List<Item>>
+    suspend fun getDislikedItems(userId: String): Result<List<Item>>
 }
