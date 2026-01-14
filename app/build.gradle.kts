dfe73e592d0fb098b9b0f547ce7dcaf301f53847
lundHannah <hannahensafilund@gmail.com>
2026-01-14 15:18:44 +0100

    Remove outdated Coil dependency from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.coil.core)
     implementation("com.github.yuyakaido:CardStackView:2.3.4")
-    implementation("io.coil-kt:coil:2.4.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
